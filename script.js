5f55eeab809ceb7c78266fdb9f4f481e2877ae31
burneearts-design <burneearts@gmail.com>
2026-02-28 14:08:43 +0300

    Add automatic slide transition functionality

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const menu = document.getElementById("menu");
 toggle.addEventListener("click", () => {
 menu.classList.toggle("active");
 });
+const slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+setInterval(() => {
+
+slides[index].classList.remove("active");
+
+index++;
+
+if(index >= slides.length){
+index = 0;
+}
+
+slides[index].classList.add("active");
+
+}, 3000);
